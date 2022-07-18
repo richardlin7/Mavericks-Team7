61c9699e25d25a8a678426a8c0b249585d1bcbb3
roshanpradhan <pradhanr@vcu.edu>
2022-07-18 18:21:14 -0400

    Revert "Revert "User controller,repository, and model class""
    This reverts commit 4006a9a13be97e022f0e8680edc9c6fae1bdb138.

---
+++ LibraryManagement/src/main/java/com/springboot/bookstore/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.springboot.bookstore.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.bookstore.repository.UserRepository;
+
+@RestController
+public class UserController {
+	
+	private UserRepository userRepository;
+
+}
