6c0da4964b1139ed9f0e5db96c08a324b39ef77f
roshanpradhan <pradhanr@vcu.edu>
2022-07-18 21:08:12 -0400

    Adding save, fetch and delete  Functionality

--- LibraryManagement/src/main/java/com/springboot/bookstore/controller/AdminController.java
+++ LibraryManagement/src/main/java/com/springboot/bookstore/controller/AdminController.java
@@ -1,14 +1,44 @@
 package com.springboot.bookstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.bookstore.model.Admin;
 import com.springboot.bookstore.repository.AdminRepository;
 
 @RestController
 public class AdminController {
-	
+
 	@Autowired
 	private AdminRepository adminRepository;
+
+	@PostMapping("/register/admin")
+	public void registerAdmin(@RequestBody Admin admin) {
+
+		adminRepository.save(admin);
+
+	}
+
+	// Showing all Admin info
+	@GetMapping("/admin")
+	public List<Admin> showAllAdmin() {
+		List<Admin> list = adminRepository.findAll();
+
+		return list;
+	}
+
+	
+	//Delete admin by ID
+	@DeleteMapping("/admin/delete/{id}")
+	public void deleteAdminById(@PathVariable("id") Long id) {
+		adminRepository.deleteById(id);
+	}
 
 }
--- LibraryManagement/src/main/java/com/springboot/bookstore/controller/UserController.java
+++ LibraryManagement/src/main/java/com/springboot/bookstore/controller/UserController.java
@@ -1,12 +1,49 @@
 package com.springboot.bookstore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.bookstore.model.User;
 import com.springboot.bookstore.repository.UserRepository;
 
 @RestController
 public class UserController {
 	
+	@Autowired
 	private UserRepository userRepository;
+	
+	
+	//Show All users
+	@GetMapping("/user")
+	public List<User> showAllUser(){
+		return userRepository.findAll();
+		
+	}
+	
+	
+	//Save Users
+	@PostMapping("/register/user")
+	public void registerUser(@RequestBody User user) {
+		
+		 userRepository.save(user);
+		
+	}
+	
+	//Delete User by ID
+	@DeleteMapping("/user/delete/{id}")
+	public void deleteUserById(@PathVariable("id") Long id) {
+		
+		userRepository.deleteById(id);
+		
+	}
+	
+	
 
 }
