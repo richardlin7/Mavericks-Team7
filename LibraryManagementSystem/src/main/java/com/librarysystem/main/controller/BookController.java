96a7fb1c36e861c068c52a33ed43e129c0a1451e
tsingh0 <taranvirsingh97@gmail.com>
2022-08-13 23:10:01 -0400

    book controller api updates

--- LibraryManagementSystem/src/main/java/com/librarysystem/main/controller/BookController.java
+++ LibraryManagementSystem/src/main/java/com/librarysystem/main/controller/BookController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,5 +46,35 @@ public class BookController {
 		
 		 bookRepository.delete(book);
 	}
+	
+	//get book by id rest api
+	@GetMapping("/book/{id}")
+	public ResponseEntity<Book> getBookById(@PathVariable Long id){
+		Book book = bookRepository.findById(id).orElseThrow();
+		return ResponseEntity.ok(book);
+	}
+	
+	//update book rest api
+	@PutMapping("/book/{id}")
+	public ResponseEntity<Book> updateBook(@PathVariable Long id,@RequestBody Book bookDetails){
+		Book book = bookRepository.findById(id).orElseThrow();
+		
+		book.setBookName(bookDetails.getBookName());
+		book.setBookStatus(bookDetails.getBookStatus());
+		book.setBookCopies(bookDetails.getBookCopies());
+		book.setBookDescription(bookDetails.getBookDescription());
+		book.setListedDate(bookDetails.getListedDate());
+		book.setAuthor(bookDetails.getAuthor());
+		book.setCategory(bookDetails.getCategory());
+		book.setLibrary(bookDetails.getLibrary());
+		
+
+
+		Book updatedBook = bookRepository.save(book);
+		
+		return ResponseEntity.ok(updatedBook);
+	}
+	 
+	
 
 }
